b8128ef397e94a37745135e2a858929b357cb74c
Fodor Ádám <adam.fodor@pm.me>
2021-01-13 01:12:06 +0100

    Add silent mode support, [WIP] Add child lock / light brightness / target humidity / function set service

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 [![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg?style=for-the-badge)](https://github.com/custom-components/hacs)
 
+Philips Air Purifier Home Assistant custom component with MQTT (for Encrypted CoAP devices).
+Tested on AC2729/50
+
+## Requirements:
+
+- [py-air-control (https://github.com/rgerganov/py-air-control)](https://github.com/rgerganov/py-air-control)
+- [CoAPthon3 (https://github.com/rgerganov/CoAPthon3)](https://github.com/rgerganov/CoAPthon3)
+
+## Installation steps:
+
+1. Add custom component to homeassitant
+2. Add the device to configuration.yml
+3. _pip3 install py-air-control_
+4. _pip3 install -U git+https://github.com/rgerganov/CoAPthon3_
+5. Restart HA
+
 ## Usage:
+
 ```yaml
 fan:
   - platform: philips-airpurifier-coap
@@ -9,36 +26,80 @@ fan:
 ```
 
 ## Configuration variables:
-Field | Value | Necessity | Description
+
+| Field    | Value                      | Necessity  | Description                  |
+| -------- | -------------------------- | ---------- | ---------------------------- |
+| platform | `philips-airpurifier-coap` | _Required_ | The platform name.           |
+| host     | 192.168.0.17               | _Required_ | IP address of your Purifier. |
+| name     | Philips Air Purifier       | Optional   | Name of the Fan.             |
+| mqtt     | 192.168.0.18               | Optional   | MQTT broker IP address       |
+
+## Here you can find device id:
+
+![](https://gitlab.com/adamcsk1-public/philips-airpurifier-coap/-/raw/master/device_id.png)
+
+---
+
+## Available functions:
+
+- Get device attributes (MQTT)
+- Set fan speed (off, low, mediem, hight, turbo)
+- Set device mode (Available in speed select: Auto / Silent)
+- Set target humidity
+- Set function
+- Set display brightness
+- Set child lock state
+
+## Available services:
+
+##### fan.philips_air_purifier_coap_set_humidity:
+
+&NewLine;
+key | Example | Possible values | Description
 --- | --- | --- | ---
-platform | `philips-airpurifier-coap` | *Required* | The platform name.
-host | 192.168.0.17 | *Required* | IP address of your Purifier.
-name | Philips Air Purifier | Optional | Name of the Fan.
-mqtt | 192.168.0.18 | Optional | MQTT broker IP address
-***
+entity_id | fan.purifier_and_humidifier | | Fan entity id
+humidity | "50" | "40", "50", "60", "70" | Tartget humidity
+
+##### fan.philips_air_purifier_coap_function:
+
+&NewLine;
+key | Example | Possible values | Description
+--- | --- | --- | ---
+entity_id | fan.purifier_and_humidifier | | Fan entity id
+function | "PH" | "P", "PH" | Device mode (P = Purification, PH = Purification and Humidification)
+
+##### fan.philips_air_purifier_coap_light_brightness:
+
+&NewLine;
+key | Example | Possible values | Description
+--- | --- | --- | ---
+entity_id | fan.purifier_and_humidifier | | Fan entity id
+brightness | "25" | "0", "25", "50", "75", "100" | Device display light brightness
+
+##### fan.philips_air_purifier_coap_child_lock:
+
+&NewLine;
+key | Example | Possible values | Description
+--- | --- | --- | ---
+entity_id | fan.purifier_and_humidifier | | Fan entity id
+state | False | True, False | Child lock state
+
+---
 
 ## MQTT topic:
-Topic | Description
---- | ---
-philips-air-purifier-coap/DEVICE_ID/attributes | Attributes json
+
+| Topic                                          | Description     |
+| ---------------------------------------------- | --------------- |
+| philips-air-purifier-coap/DEVICE_ID/attributes | Attributes json |
 
 ## MQTT topic sensor usage:
+
 ```yaml
 sensor:
   - platform: mqtt
-    state_topic: "philips-air-purifier-coap/DEVICE_ID/attributes"
-    name: "PM 2.5 (Airpurifier)"
-    unit_of_measurement: "Index"
-    icon: "mdi:chart-line"
-    value_template: "{{ value_json.pm25 }}"
+    state_topic: 'philips-air-purifier-coap/DEVICE_ID/attributes'
+    name: 'PM 2.5 (Airpurifier)'
+    unit_of_measurement: 'Index'
+    icon: 'mdi:chart-line'
+    value_template: '{{ value_json.pm25 }}'
 ```
-
-## Installation steps:
-1. Add custom component to homeassitant
-2. Add the device to configuration.yml
-3. pip3 install py-air-control
-4. pip3 install -U git+https://github.com/rgerganov/CoAPthon3
-5. Restart HA
-
-## Here you can find device id:
-![](https://gitlab.com/adamcsk1-public/philips-airpurifier-coap/-/raw/master/device_id.png)
--- custom_components/philips-airpurifier-coap/fan.py
+++ custom_components/philips-airpurifier-coap/fan.py
@@ -5,14 +5,14 @@ import paho.mqtt.client as mqtt
 import logging
 import json
 import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers import config_validation as cv, service
 from homeassistant.components.fan import FanEntity, PLATFORM_SCHEMA, SPEED_HIGH, SPEED_LOW, SPEED_MEDIUM, SPEED_OFF, SUPPORT_SET_SPEED
 from . import interval as setInterval
 
 _LOGGER = logging.getLogger(__name__)
 _MQTT_CLIENT = mqtt.Client(__name__)
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 CONF_HOST = "host"
 CONF_NAME = "name"
@@ -21,6 +21,15 @@ CONF_MQTT = "mqtt"
 DOMAIN = "philips-air-purifier-coap"
 DEFAULT_NAME = "Philips AirPurifier"
 ICON = "mdi:air-purifier"
+SERVICE_PREFIX = "philips_air_purifier_coap_set"
+
+MODE_ALLERGEN = "allergen"
+MODE_AUTO = "auto"
+SPEED_TURBO = "turbo"
+SPEED_SILENT = "silent"
+HUMIDITY_OPTIONS = ["40","50","60","70"]
+FUNCTION_OPTIONS = ["P","PH"]
+BRIGHTNESS_OPTIONS = ["0","25","50","75","100"]
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
@@ -30,9 +39,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     }
 )
 
-
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    add_devices([PhilipsAirPurifierCoapFan(hass, config)])
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    add_entities([PhilipsAirPurifierCoapFan(hass, config)])
 
 class PhilipsAirPurifierCoapFan(FanEntity):
     def __init__(self, hass, config):
@@ -45,6 +53,26 @@ class PhilipsAirPurifierCoapFan(FanEntity):
         self._online = False
         self._inf_loop = 0
 
+        hass.services.register("fan", SERVICE_PREFIX+"_humidity", self.set_humidity, schema = vol.Schema({
+            vol.Required("entity_id"): cv.entity_id,
+            vol.Required("humidity"): vol.In(HUMIDITY_OPTIONS)
+        }))
+
+        hass.services.register("fan", SERVICE_PREFIX+"_function", self.set_function, schema = vol.Schema({
+            vol.Required("entity_id"): cv.entity_id,
+            vol.Required("function"): vol.In(FUNCTION_OPTIONS)
+        }))
+
+        hass.services.register("fan", SERVICE_PREFIX+"_light_brightness", self.set_light_brightness, schema = vol.Schema({
+            vol.Required("entity_id"): cv.entity_id,
+            vol.Required("brightness"): vol.In(BRIGHTNESS_OPTIONS)
+        }))
+
+        hass.services.register("fan", SERVICE_PREFIX+"_child_lock", self.set_child_lock, schema = vol.Schema({
+            vol.Required("entity_id"): cv.entity_id,
+            vol.Required("state"): cv.boolean
+        }))
+
         if config[CONF_MQTT]:
             _MQTT_CLIENT.connect(config[CONF_MQTT])
             self._mqtt_initialized = True
@@ -52,6 +80,7 @@ class PhilipsAirPurifierCoapFan(FanEntity):
         setInterval.Interval(60, self._update_attributes)
 
         self._update_attributes()
+
 
     def _mqtt_send(self, key, value):
         if self._attr["device_id"]:
@@ -83,65 +112,6 @@ class PhilipsAirPurifierCoapFan(FanEntity):
         except Exception as e:
             _LOGGER.error("Unexpected error:{}".format(e))
             self._online = False
-
-    @property
-    def should_poll(self) -> bool:
-        return True
-
-    @property
-    def speed(self):
-        try:
-            if self._attr["mode"] == "auto":
-                return "auto"
-            if self._attr["mode"] == "allergen":
-                return "allergen"
-            if self._attr["fan_speed"] == "0":
-                return SPEED_OFF
-            if self._attr["fan_speed"] == "1":
-                return SPEED_LOW
-            if self._attr["fan_speed"] == "2":
-                return SPEED_MEDIUM
-            if self._attr["fan_speed"] == "3":
-                return SPEED_HIGH
-            if self._attr["fan_speed"] == "turbo":
-                return "turbo"
-            return None
-        except Exception as e:
-        	_LOGGER.error("Unexpected error:{}".format(e))
-        	return None
-
-    @property
-    def fan_speed(self):
-        return self.speed()
-
-    @property
-    def state(self):
-        try:
-            if self._online == False:
-               return None
-            if self._attr["state"] == "ON":
-                return "on"
-            else:
-                return "off"
-        except Exception as e:
-        	_LOGGER.error("Unexpected error:{}".format(e))
-        	return None
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def icon(self):
-        return ICON
-
-    @property
-    def supported_features(self) -> int:
-        return SUPPORT_SET_SPEED
-
-    @property
-    def speed_list(self):
-        return [SPEED_OFF, SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH, "turbo", "auto", "allergen"]
 
     def _action_send_failed(self, stdout):
         if stdout.find('failed') == -1 or self._inf_loop > 10:
@@ -184,13 +154,56 @@ class PhilipsAirPurifierCoapFan(FanEntity):
             stdout =  self._run(self._cmd + " --mode M --om 2 --debug")
         if speed == SPEED_HIGH:
             stdout = self._run(self._cmd + " --mode M --om 3 --debug")
-        if speed == "turbo":
+        if speed == SPEED_TURBO:
             stdout = self._run(self._cmd + " --mode M --om t --debug")
-        if speed == "auto":
+        if speed == MODE_AUTO:
             stdout = self._run(self._cmd + " --mode P --debug")
-        if speed == "allergen":
+        if speed == MODE_ALLERGEN:
             stdout = self._run(self._cmd + " --mode A --debug")
+        if speed == SPEED_SILENT:
+            stdout = self._run(self._cmd + " --mode S --debug")
         return stdout
+
+    def set_humidity(self, call):
+        if self.entity_id == call.data.get("entity_id"):
+            stdout = self._run(self._cmd + " --rhset " + call.data.get("humidity") + " --debug")
+
+            if self._action_send_failed(stdout):
+                self.set_humidity(call)
+            else:
+                self._update_attributes()
+
+    def set_function(self, call):
+        if self.entity_id == call.data.get("entity_id"):
+            stdout = self._run(self._cmd + " --func " + call.data.get("function") + " --debug")
+
+            if self._action_send_failed(stdout):
+                self.set_function(call)
+            else:
+                self._update_attributes()
+
+    def set_light_brightness(self, call):
+        if self.entity_id == call.data.get("entity_id"):
+            stdout = self._run(self._cmd + " --aqil " + call.data.get("brightness") + " --debug")
+
+            if self._action_send_failed(stdout):
+                self.set_light_brightness(call)
+            else:
+                self._update_attributes()
+
+    def set_child_lock(self, call):
+        if self.entity_id == call.data.get("entity_id"):
+            if call.data.get("state") == True:
+                cl = "True"
+            else:
+                cl = "False"
+
+            stdout = self._run(self._cmd + " --cl " + cl + " --debug")
+
+            if self._action_send_failed(stdout):
+                self.set_child_lock(call)
+            else:
+                self._update_attributes()
 
     def _update_attributes(self):
         stdout = self._run(self._cmd)
@@ -240,7 +253,66 @@ class PhilipsAirPurifierCoapFan(FanEntity):
         if self._mqtt_initialized:
             self._mqtt_send("attributes", json.dumps(self._attr))
 
+    def update(self):
+        self._device_available()
+
+    @property
+    def speed(self):
+        try:
+            if self._attr["mode"] == MODE_AUTO:
+                return MODE_AUTO
+            if self._attr["mode"] == MODE_ALLERGEN:
+                return MODE_ALLERGEN
+            if self._attr["fan_speed"] == "0":
+                return SPEED_OFF
+            if self._attr["fan_speed"] == "1":
+                return SPEED_LOW
+            if self._attr["fan_speed"] == "2":
+                return SPEED_MEDIUM
+            if self._attr["fan_speed"] == "3":
+                return SPEED_HIGH
+            if self._attr["fan_speed"] == SPEED_TURBO:
+                return SPEED_TURBO
+            if self._attr["fan_speed"] == SPEED_SILENT:
+                return SPEED_SILENT
+            return None
+        except Exception as e:
+        	_LOGGER.error("Unexpected error:{}".format(e))
+        	return None
+
+    @property
+    def fan_speed(self):
+        return self.speed()
+
+    @property
+    def state(self):
+        try:
+            if self._online == False:
+               return None
+            if self._attr["state"] == "ON":
+                return "on"
+            else:
+                return "off"
+        except Exception as e:
+        	_LOGGER.error("Unexpected error:{}".format(e))
+        	return None
+
     @property
     def device_state_attributes(self):
-        self._device_available()
         return self._attr
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def icon(self):
+        return ICON
+
+    @property
+    def supported_features(self) -> int:
+        return SUPPORT_SET_SPEED
+
+    @property
+    def speed_list(self):
+        return [SPEED_OFF, SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH, SPEED_TURBO, MODE_ALLERGEN, MODE_AUTO, SPEED_SILENT]
